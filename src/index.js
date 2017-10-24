2c4abd76f75ce1a0d653a5a1f657451c32c2069b
viktor.romanyuk <viktor.romanyuk@aisnovations.com>
2017-10-24 11:47:14 +0300

    redux basics also with devtools(logger)

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {render} from 'react-dom'
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import logger from 'redux-logger'
+import todoApp from './reducers'
+import App from './components/App'
+
+const store = createStore(todoApp, composeWithDevTools(applyMiddleware(logger)));
+
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+)
